505d5c97c27c1c557c0bc768141c2319f87f392e
Anuj <anujarya6121998@gmail.com>
2021-05-27 00:06:55 +0530

    add some changes

--- Banks/settings.py
+++ Banks/settings.py
@@ -139,6 +139,7 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static').
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
