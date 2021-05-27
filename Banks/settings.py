a5e1cfc36a2f5192c30f940737622bbcc5073378
Anuj <anujarya6121998@gmail.com>
2021-05-27 12:01:21 +0530

    sample commit

--- Banks/settings.py
+++ Banks/settings.py
@@ -104,9 +104,9 @@ DATABASES = {
 #     }
 # }
 
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -145,10 +145,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'Banks/static')
-]
 
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
