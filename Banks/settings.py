79f0187f7b8ae8c317f05c1a698ed7cd9bc7b5c4
Anuj <anujarya6121998@gmail.com>
2021-05-27 11:44:40 +0530

    set heroku environment

--- Banks/settings.py
+++ Banks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,13 +140,19 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static').
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'Banks/static')
+]
+
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     'SEARCH_PARAM':'q'
 }
+
+django_heroku.settings(locals())
